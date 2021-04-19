3371269d09250fb526e22c1927c5c1f4d143c71c
João Henrique <jou.098olo@gmail.com>
2021-04-19 11:11:28 -0300

    criando operação de instanciação

--- src/main/java/com/estagiobetha/CursomcApplication.java
+++ src/main/java/com/estagiobetha/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.estagiobetha;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.estagiobetha.domain.Categoria;
+import com.estagiobetha.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "INFORMÁTICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
