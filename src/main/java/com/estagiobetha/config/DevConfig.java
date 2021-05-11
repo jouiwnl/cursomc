2c873fde88c68fbb7eb89f6e7c16313995fdb790
João Henrique <jou.098olo@gmail.com>
2021-05-11 10:19:42 -0300

    Reconfigurando postgres/Endpoints funcionando/Deploy em produção

--- src/main/java/com/estagiobetha/config/DevConfig.java
+++ src/main/java/com/estagiobetha/config/DevConfig.java
@@ -1,5 +1,7 @@
 package com.estagiobetha.config;
 
+import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,8 @@ public class DevConfig {
 	private DBService dbService;
 	
 	@Bean
-	public boolean instantiateDatabase() {
-		
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
 		return true;
 	}
 	
